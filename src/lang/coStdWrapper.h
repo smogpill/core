060d44ec58e9ce0fd944d68290d46c216c27d24f
Jounayd Id Salah <smogpill@gmail.com>
2016-07-25 00:34:51 +0200

    Fixed clang compatibility issue with automatic type deduction for return types.

--- src/lang/coStdWrapper.h
+++ src/lang/coStdWrapper.h
@@ -2,7 +2,7 @@
 // Distributed under the MIT License (See accompanying file LICENSE.md file or copy at http://opensource.org/licenses/MIT).
 #pragma once
 
-template <class T> coFORCE_INLINE auto begin(T& _this) { return coBegin(_this); }
-template <class T> coFORCE_INLINE auto begin(const T& _this) { return coBegin(_this); }
-template <class T> coFORCE_INLINE auto end(T& _this) { return coEnd(_this); }
-template <class T> coFORCE_INLINE auto end(const T& _this) { return coEnd(_this); }
+template <class T> coFORCE_INLINE auto begin(T& _this) -> decltype(coBegin(_this)) { return coBegin(_this); }
+template <class T> coFORCE_INLINE auto begin(const T& _this) -> decltype(coBegin(_this)) { return coBegin(_this); }
+template <class T> coFORCE_INLINE auto end(T& _this) -> decltype(coEnd(_this)) { return coEnd(_this); }
+template <class T> coFORCE_INLINE auto end(const T& _this) -> decltype(coEnd(_this)) { return coEnd(_this); }
