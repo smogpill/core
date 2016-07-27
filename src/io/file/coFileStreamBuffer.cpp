570687399fb734ed8e90b8fad7fe1a07e162efef
Jounayd Id Salah <smogpill@gmail.com>
2016-07-27 21:13:09 +0200

    Added coFileStreamBuffer implementation.

--- src/io/file/coFileStreamBuffer.cpp
+++ src/io/file/coFileStreamBuffer.cpp
@@ -2,3 +2,87 @@
 // Distributed under the MIT License (See accompanying file LICENSE.md file or copy at http://opensource.org/licenses/MIT).
 #include "io/pch.h"
 #include "io/file/coFileStreamBuffer.h"
+#include "io/file/coFileAccess.h"
+#include "lang/result/coResult_f.h"
+#include "lang/reflect/coNumericLimits.h"
+
+coFileStreamBuffer::coFileStreamBuffer()
+	: fileAccess(nullptr)
+{
+
+}
+
+coFileStreamBuffer::~coFileStreamBuffer()
+{
+	Flush();
+}
+
+coResult coFileStreamBuffer::OnInit(const coObject::InitConfig& _config)
+{
+	coTRY(Super::OnInit(_config), nullptr);
+	const InitConfig& config = static_cast<const InitConfig&>(_config);
+
+	fileAccess = config.fileAccess;
+	coTRY(fileAccess, nullptr);
+
+
+
+	return true;
+}
+
+void coFileStreamBuffer::Reset()
+{
+	Super::Reset();
+	cursor = windowBegin;
+	coWARN_NOT_AVAILABLE();
+}
+
+void coFileStreamBuffer::Flush()
+{
+	Super::Flush();
+	if (fileAccess)
+	{
+		const coFileAccess::Mode mode = fileAccess->GetMode();
+		if (mode == coFileAccess::Mode::write)
+		{
+			coASSERT(cursor - windowBegin < coNumericLimits<coUint>::Max());
+			const coUint remainingSize8 = static_cast<coUint>(cursor - windowBegin);
+			result &= fileAccess->Write(buffer.data, remainingSize8);
+			cursor = windowBegin;
+		}
+
+		result &= fileAccess->Flush();
+	}
+}
+
+coBool coFileStreamBuffer::RefillRead()
+{
+	cursor = windowBegin;
+	coUint readSize8;
+	if (fileAccess->Read(readSize8, buffer.data, buffer.count))
+	{
+		windowEnd = windowBegin + readSize8;
+		return true;
+	}
+	else
+	{
+		coERROR("Failed to read more data from the file " << fileAccess->GetDebugName());
+		SetErrorMode();
+		return false;
+	}
+}
+
+coBool coFileStreamBuffer::RefillWrite()
+{
+	cursor = windowBegin;
+	if (fileAccess->Write(buffer.data, buffer.count))
+	{
+		return true;
+	}
+	else
+	{
+		coERROR("Failed to write data to the file " << fileAccess->GetDebugName());
+		SetErrorMode();
+		return false;
+	}
+}
--- src/io/file/coFileStreamBuffer.h
+++ src/io/file/coFileStreamBuffer.h
@@ -1,3 +1,35 @@
 // Copyright(c) 2016 Jounayd Id Salah
 // Distributed under the MIT License (See accompanying file LICENSE.md file or copy at http://opensource.org/licenses/MIT).
 #pragma once
+
+#include "io/stream/coStreamBuffer.h"
+
+class coFileAccess;
+
+class coFileStreamBuffer : public coStreamBuffer
+{
+	coDECLARE_SUPER(coStreamBuffer);
+public:
+	coFileStreamBuffer();
+	~coFileStreamBuffer();
+
+	class InitConfig : public Super::InitConfig
+	{
+	public:
+		InitConfig();
+		coFileAccess* fileAccess;
+	};
+
+	virtual void Reset() override;
+	virtual void Flush() override;
+
+protected:
+	virtual coResult OnInit(const coObject::InitConfig& _config) override;
+
+private:
+	coBool RefillRead();
+	coBool RefillWrite();
+
+	coFileAccess* fileAccess;
+	coDynamicArray<coByte> buffer;
+};
