956cfe9e3833ae4f831c0bb484082864af3775ee
Jounayd Id Salah <smogpill@gmail.com>
2016-07-31 00:27:11 +0200

    Added coGetType<T>().

--- src/lang/reflect/coType_f.h
+++ src/lang/reflect/coType_f.h
@@ -9,3 +9,54 @@ struct coNestedReflectedCheck
 };*/
 
 class coType;
+
+template <class T>
+coInt32 _coTestNestedReflectedAttribute(typename T::_attribute_Reflected*);
+template <class>
+coInt8 _coTestNestedReflectedAttribute(...);
+
+template <class T, coBool>
+struct _coTypeOf
+{
+	static const coType* Get() { return nullptr; }
+};
+
+template <class T>
+struct _coTypeOf<T, true>
+{
+	static const coType* Get() { return T::staticType; }
+};
+
+template <class T>
+const coType* coGetType()
+{
+	return _coTypeOf<T, sizeof(decltype(_coTestNestedReflectedAttribute<T>(0))) == sizeof(coInt32)>::Get();
+}
+
+template <>
+const coType* coGetType<coBool>()
+{
+	return nullptr;
+}
+
+
+// coBool coIsSuper(const coType& _this, const coType& _derived)
+// {
+// 
+// }
+
+template <class T, class U>
+coBool coIsTypeCompatible(const U&)
+{
+	const coType* otherType = coGetType<T>();
+	if (otherType == nullptr)
+		return false;
+	const coType* type = coGetType<U>();
+	while (type != nullptr)
+	{
+		if (type == otherType)
+			return true;
+		type = type->super;
+	}
+	return false;
+}
