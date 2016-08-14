8b093bbc5e01508f15b8e73a08a5dfdd26c6ffe1
Jounayd Id Salah <smogpill@gmail.com>
2016-08-14 21:44:17 +0200

    Added Object type info in Vulkan messages.

--- src/render_vulkan/coVulkanMessageHandler.cpp
+++ src/render_vulkan/coVulkanMessageHandler.cpp
@@ -7,7 +7,43 @@
 #include "lang/result/coResult_f.h"
 #include "memory/allocator/coLocalAllocator.h"
 
-static VkBool32 coDebugCallback_vk(VkDebugReportFlagsEXT _flags, VkDebugReportObjectTypeEXT /*_objType*/, uint64_t /*_obj*/,
+static const coConstString _coGetDebugReportObjectTypeString(const VkDebugReportObjectTypeEXT& _type)
+{
+	switch (_type)
+	{
+	case VK_DEBUG_REPORT_OBJECT_TYPE_INSTANCE_EXT: return "VkInstance";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_PHYSICAL_DEVICE_EXT: return "VkPhysicalDevice";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_DEVICE_EXT: return "VkDevice";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_QUEUE_EXT: return "VkQueue";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_SEMAPHORE_EXT: return "VkSemaphore";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_COMMAND_BUFFER_EXT: return "VkCommandBuffer";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_FENCE_EXT: return "VkFence";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_DEVICE_MEMORY_EXT: return "VkDeviceMemory";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_BUFFER_EXT: return "VkBuffer";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_IMAGE_EXT: return "VkImage";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_EVENT_EXT: return "VkEvent";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_QUERY_POOL_EXT: return "VkQueryPool";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_BUFFER_VIEW_EXT: return "VkBufferView";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_IMAGE_VIEW_EXT: return "VkImageView";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_SHADER_MODULE_EXT: return "VkShaderModule";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_PIPELINE_CACHE_EXT: return "VkPipelineCache";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_PIPELINE_LAYOUT_EXT: return "VkPipelineLayout";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_RENDER_PASS_EXT: return "VkRenderPass";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_PIPELINE_EXT: return "VkPipeline";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_DESCRIPTOR_SET_LAYOUT_EXT: return "VkDescriptorSetLayout";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_SAMPLER_EXT: return "VkSampler";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_DESCRIPTOR_POOL_EXT: return "VkDescriptorPool";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_DESCRIPTOR_SET_EXT: return "VkDescriptorSet";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_FRAMEBUFFER_EXT: return "VkFramebuffer";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_COMMAND_POOL_EXT: return "VkCommandPool";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_SURFACE_KHR_EXT: return "VkSurfaceKHR";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_SWAPCHAIN_KHR_EXT: return "VkSwapchainKHR";
+	case VK_DEBUG_REPORT_OBJECT_TYPE_DEBUG_REPORT_EXT: return "VkDebugReport";
+	default: return "<unknown>";
+	}
+}
+
+static VkBool32 _coDebugCallback_vk(VkDebugReportFlagsEXT _flags, VkDebugReportObjectTypeEXT _objType, uint64_t /*_obj*/,
 	size_t /*_location*/, int32_t /*_code*/, const char* _layerPrefix, const char* _msg, void* /*_userData*/)
 {
 	coLocalAllocator localAllocator(2048);
@@ -15,7 +51,7 @@ static VkBool32 coDebugCallback_vk(VkDebugReportFlagsEXT _flags, VkDebugReportOb
 	prefix << "Vulkan";
 	if (_layerPrefix)
 	{
-		prefix << " (layer: " << _layerPrefix << ")";
+		prefix << " (objectType: " << _coGetDebugReportObjectTypeString(_objType) << ", layer: " << _layerPrefix << ") ";
 	}
 
 	if (_flags & VK_DEBUG_REPORT_ERROR_BIT_EXT)
@@ -70,7 +106,7 @@ coResult coVulkanMessageHandler::OnInit(const coObject::InitConfig& _config)
 	VkDebugReportCallbackCreateInfoEXT createInfo = {};
 	createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
 	createInfo.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT | VK_DEBUG_REPORT_WARNING_BIT_EXT | VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT | VK_DEBUG_REPORT_INFORMATION_BIT_EXT | VK_DEBUG_REPORT_DEBUG_BIT_EXT;
-	createInfo.pfnCallback = (PFN_vkDebugReportCallbackEXT)coDebugCallback_vk;
+	createInfo.pfnCallback = (PFN_vkDebugReportCallbackEXT)_coDebugCallback_vk;
 	coVULKAN_TRY(createFunc(*instance_vk, &createInfo, nullptr, &callback_vk), "Failed to create the message handler callback");
 	return true;
 }
