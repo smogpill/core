eb9cdee09d5e495dc46846d9be9c9873623dfde4
Jounayd Id Salah <smogpill@gmail.com>
2017-08-03 00:03:56 +0200

    tanh

--- src/neural/process/coNeuralActivation_f.h
+++ src/neural/process/coNeuralActivation_f.h
@@ -4,23 +4,33 @@
 
 #include "math/scalar/coFloat_f.h"
 
+#define coNEURAL_TANH_ACTIVATION 0
+#define coNEURAL_LOGISTIC_ACTIVATION 1
+#define coNEURAL_CURRENT_ACTIVATION coNEURAL_TANH_ACTIVATION
+
 coFORCE_INLINE coFloat coComputeNeuralActivation(coFloat _x)
 {
+#if coNEURAL_CURRENT_ACTIVATION == coNEURAL_TANH_ACTIVATION
+	// Tanh is better than the usual logistic function because it avoids saturation at near 0. 
+	// (output: [-1, 1])
+	//return 2.0f / (1.0f + coExp(-2.0f * _x)) - 1.0f;
+	const coFloat a = coExp(2.0f * _x);
+	return (a - 1.0f) / (a + 1.0f);
+#elif coNEURAL_CURRENT_ACTIVATION == coNEURAL_LOGISTIC_ACTIVATION
+	// Logistic
+	// (output: [0, 1], not good)
 	return 1.0f / (1.0f + coExp(-_x));
-
-	// Not used for now, because:
-	// - Have to find a good derivative form
-	// - Have to understand how the range change from [0, 1] to [-1, 1] affects the inputs and outputs.
-	// tanh = (exp(2x) - 1) / (exp(2x) + 1)
-	// tanh is better than the usual logistic function because it avoids saturation at near 0. 
-	// 	const coFloat a = 2.0f * _x;
-	// 	const coFloat b = coExp(a);
-	// 	return (b - 1.0f) / (b + 1.0f);
+#endif
 }
 
-// Not the direct activation derivative, this takes the output as a parameter
+// Not the direct activation derivative, this takes the output as a parameter. Caching trick.
 coFORCE_INLINE coFloat coComputeNeuralActivationDerivativeTransfer(coFloat _output)
 {
-	// The derivative is coComputeNeuralActivation(x) * (1 - coComputeNeuralActivation(x))
-	return _output * (1 - _output);
+#if coNEURAL_CURRENT_ACTIVATION == coNEURAL_TANH_ACTIVATION
+	// The derivative is 1 - (coComputeNeuralActivation(x))^2
+	return 1.0f - _output * _output; // tanh
+#elif coNEURAL_CURRENT_ACTIVATION == coNEURAL_LOGISTIC_ACTIVATION
+	// The derivative is coComputeNeuralActivation(x) * (1 - coComputeNeuralActivation(x)) 
+	return _output * (1 - _output); // logistic
+#endif
 }
