69c40ebb6fd98258a9b7e0f0707c335709acbc0c
Jounayd Id Salah <smogpill@gmail.com>
2017-09-23 16:45:39 +0200

    imgui files

--- src/gui_imgui/premake5.lua
+++ src/gui_imgui/premake5.lua
@@ -1,1 +1,6 @@
 coSetCppProjectDefaults("gui_imgui")
+
+--imguiPath = externalAbsPath .. "/imgui"
+--files { imguiPath .. "/src" }
+--vpaths { ["imgui"] = imguiPath .. "/*" }
+--includedirs {imguiPath}
--- src/runtime/premake5.lua
+++ src/runtime/premake5.lua
@@ -1,6 +1,9 @@
 coSetCppProjectDefaults("runtime")
 kind "WindowedApp"
-coSetProjectDependencies{"lang", "debug", "app", "pattern", "memory", "container", "io", "platform", "math", "render", "render_vulkan", "shader", "parser", "gui", "gui_imgui"}
+coSetProjectDependencies{"lang", "debug", "app", "pattern", "memory", 
+"container", "io", "platform", "math", "render", "render_vulkan", "shader", 
+"parser", "gui", "imgui", "gui_imgui"}
 --links {"lang", "debug", "app", "pattern", "memory", "container", "io", "platform", "math", "render", "render_vulkan"}
 libdirs "$(VULKAN_SDK)/Bin"
 links {"vulkan-1.lib"}
+
