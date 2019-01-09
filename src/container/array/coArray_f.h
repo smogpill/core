78095e53eaaebbeacb642eae3f733d04885bc538
Jounayd Id Salah <smogpill@gmail.com>
2019-01-09 23:48:57 +0100

    stuff

--- src/container/array/coArray_f.h
+++ src/container/array/coArray_f.h
@@ -255,3 +255,26 @@ void coShuffle(coArray<T>& _this, coUint32& _seed)
 		coSwap(p[i], p[coRand(_seed, i + 1)]);
 	}
 }
+
+template <class T>
+coBool operator== (const coArray<T>& _a, const coArray<T>& _b)
+{
+	if (_a.count != _b.count)
+	{
+		return false;
+	}
+	const T* dataA = _a.data;
+	const T* dataB = _b.data;
+	for (coUint i = 0; i < _a.count; ++i)
+	{
+		if (dataA[i] != dataB[i])
+			return false;
+	}
+	return true;
+}
+
+template <class T>
+coBool operator!= (const coArray<T>& _a, const coArray<T>& _b)
+{
+	return !(_a == _b);
+}
