322ebb0e338e620a9398fd81738558df3ef2345b
Jounayd Id Salah <smogpill@gmail.com>
2019-12-27 02:00:35 +0100

    Fixed cpu float flags.

--- src/math/scalar/coFloat_f.cpp
+++ src/math/scalar/coFloat_f.cpp
@@ -2,28 +2,22 @@
 // Distributed under the MIT License (See accompanying file LICENSE.md file or copy at http://opensource.org/licenses/MIT).
 #include "math/pch.h"
 #include "math/scalar/coFloat_f.h"
+#include <debug/log/coLog.h>
 
 void coSetDenormals(coBool b)
 {
-	if (b)
+	const errno_t err = _controlfp_s(nullptr, b ? _DN_SAVE : _DN_FLUSH, _MCW_DN);
+	if (err)
 	{
-		_controlfp_s(NULL, _DN_SAVE, _MCW_DN);
-	}
-	else
-	{
-		_controlfp_s(NULL, _DN_FLUSH, _MCW_DN);
+		coERROR("coSetDenormals(" << b << "). Error: " << err);
 	}
 }
 
 void coSetFloatingPointExceptions(coBool b)
 {
-	if (b)
+	const errno_t err = _controlfp_s(nullptr, b ? 0 : _MCW_EM, _MCW_EM); // 1 hides the exception, 0 sets it.
+	if (err)
 	{
-		unsigned int fp_control_state;
-		_controlfp_s(&fp_control_state, _EM_INEXACT, _MCW_EM);
-	}
-	else
-	{
-		coASSERT(false);
+		coERROR("coSetFloatingPointExceptions(" << b << "). Error: " << err);
 	}
 }
