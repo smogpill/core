e42b225b0d164b05a8625bc5fbbf5a6b5bc65ca7
Jounayd Id Salah <smogpill@gmail.com>
2021-09-05 17:43:51 +0200

    sdf

--- src/math/shape/coIntersection_f.h
+++ src/math/shape/coIntersection_f.h
@@ -2,6 +2,8 @@
 // Distributed under the MIT License (See accompanying file LICENSE.md file or copy at http://opensource.org/licenses/MIT).
 #pragma once
 #include "../vector/coVec3.h"
+#include "math/shape/coSegment_f.h"
+#include "math/shape/coPlane_f.h"
 
 coFORCE_INLINE coFloat coSigned2DTriArea(const coVec3& a, const coVec3& b, const coVec3& c)
 {
@@ -24,5 +26,17 @@ inline coBool coIntersectSegmentSegment(const coVec3& a, const coVec3& b, const 
 		}
 	}
 
+	return false;
+}
+
+coFORCE_INLINE coBool coIntersect(const coPlane& plane, const coSegment& seg, coVec3& p)
+{
+	const coVec3 delta = seg.p1 - seg.p0;
+	const coFloatx4 t = (coBroadcastW(plane.normalAndDistance) - coDot(coVec3(plane.normalAndDistance), seg.p0)) / coDot(coVec3(plane.normalAndDistance), delta);
+	if (t >= 0.0f && t <= 1.0f)
+	{
+		p = seg.p0 + t * delta;
+		return true;
+	}
 	return false;
 }
