a03e101e7813e3b1b9a53e1a51aee007b7b425b2
Jounayd Id Salah <smogpill@gmail.com>
2021-10-29 01:28:06 +0200

    wer

--- src/container/array/coDynamicArray_f.h
+++ src/container/array/coDynamicArray_f.h
@@ -37,6 +37,7 @@ void coReserve(coDynamicArray<T>& _this, coUint32 _desiredCount)
 		// - For simplicity. For example for working using SIMD on a float array.
 		// - Seems faster on Intel architectures https://software.intel.com/en-us/articles/data-alignment-when-migrating-to-64-bit-intel-architecture).
 		const coUint alignment = alignof(T) > 16 ? alignof(T) : 16;
+		coASSERT(coUint64(bestCapacity) * sizeof(T) & 0xffffffff);
 		T* newBuffer = static_cast<T*>(coAllocator::GetHeap()->AllocateAligned(bestCapacity * sizeof(T), alignment));
 		if (_this.data)
 		{
