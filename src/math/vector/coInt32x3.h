e398d3d33dc767b9515d601aec63557bb2b0a63b
Jounayd Id Salah <smogpill@gmail.com>
2021-11-13 01:48:26 +0100

    dfgdfg

--- src/math/vector/coInt32x3.h
+++ src/math/vector/coInt32x3.h
@@ -1,7 +1,6 @@
 // Copyright(c) 2016 Jounayd Id Salah
 // Distributed under the MIT License (See accompanying file LICENSE.md file or copy at http://opensource.org/licenses/MIT).
 #pragma once
-
 #include "lang/reflect/coTypeDecl.h"
 
 class alignas(16) coInt32x3
---
+++ src/pattern/clock/coClock.h
@@ -0,0 +1,11 @@
+// Copyright(c) 2021 Jounayd Id Salah
+#pragma once
+
+class coClock
+{
+public:
+	void Step();
+
+	coFloat dt = 0.0f;
+	coFloat64 time = 0.0f;
+};
