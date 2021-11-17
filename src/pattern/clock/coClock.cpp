62f09a4401dc0d911296d5cb230a1883ef54dd34
Jounayd Id Salah <smogpill@gmail.com>
2021-11-17 23:20:35 +0100

    xcsdf

--- src/pattern/clock/coClock.cpp
+++ src/pattern/clock/coClock.cpp
@@ -2,3 +2,30 @@
 // Distributed under the MIT License (See accompanying file LICENSE.md file or copy at http://opensource.org/licenses/MIT).
 #include "pattern/pch.h"
 #include "coClock.h"
+#include <platform/coOs.h>
+#include <math/scalar/coFloat_f.h>
+
+coClock::coClock()
+{
+	rootTimeStamp = coGetTimeStamp();
+	lastTimeStamp = rootTimeStamp;
+}
+
+void coClock::Step()
+{
+	const coUint64 timeStamp = coGetTimeStamp();
+	if (fixedDt >= 0.0f)
+	{
+		rawDt = fixedDt;
+		dt = fixedDt;
+		time += fixedDt;
+	}
+	else
+	{
+		const coFloat64 newTime = coGetSeconds(timeStamp - rootTimeStamp);
+		rawDt = coFloat(newTime - time);
+		dt = coMin(rawDt, maxDt);
+		time = newTime;
+	}
+	lastTimeStamp = timeStamp;
+}
--- src/pattern/clock/coClock.h
+++ src/pattern/clock/coClock.h
@@ -4,8 +4,20 @@
 class coClock
 {
 public:
+	coClock();
 	void Step();
+	void SetFixedDt(coFloat fixedDt_) { fixedDt = fixedDt_; }
+	coFloat GetDt() const { return dt; }
+	coFloat GetRawDt() const { return rawDt; }
+	coFloat64 GetTime() const { return time; }
+
+private:
+	coFloat fixedDt = 0.0f;
+	coFloat maxDt = 1.0f / 10.0f;
 
 	coFloat dt = 0.0f;
 	coFloat64 time = 0.0f;
+	coFloat rawDt = 0.0f;
+	coUint64 lastTimeStamp = 0;
+	coUint64 rootTimeStamp = 0;
 };
