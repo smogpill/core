55d59201e9d509152a307febf33037ae2680cb58
Jounayd Id Salah <smogpill@gmail.com>
2021-12-30 19:58:28 +0100

    fghfgh

--- src/math/vector/coInt32x3.h
+++ src/math/vector/coInt32x3.h
@@ -2,6 +2,7 @@
 // Distributed under the MIT License (See accompanying file LICENSE.md file or copy at http://opensource.org/licenses/MIT).
 #pragma once
 #include "lang/reflect/coTypeDecl.h"
+class coFloatx3;
 
 class alignas(16) coInt32x3
 {
@@ -10,6 +11,7 @@ public:
 	coFORCE_INLINE coInt32x3() {}
 	coFORCE_INLINE coInt32x3(coInt32 _xyz) : x(_xyz), y(_xyz), z(_xyz) {}
 	coFORCE_INLINE coInt32x3(coInt32 _x, coInt32 _y, coInt32 _z) : x(_x), y(_y), z(_z) {}
+	coFORCE_INLINE explicit coInt32x3(const coFloatx3& f) { coBitCast<__m128i>(*this) = _mm_cvttps_epi32(coBitCast<__m128>(f)); }
 	coFORCE_INLINE coInt32& operator[](coUint _i) { return (&x)[_i]; }
 	coFORCE_INLINE coInt32 operator[](coUint _i) const { return (&x)[_i]; }
 
