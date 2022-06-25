43d54118f8f703f2f17da66687e40a6c50db9e6b
Jounayd Id Salah <smogpill@gmail.com>
2022-06-25 13:25:02 +0200

    Fixed flat vertex dissolve.

--- src/math/topology/halfEdge/dissolve/coDissolveFlatVertices_f.cpp
+++ src/math/topology/halfEdge/dissolve/coDissolveFlatVertices_f.cpp
@@ -16,68 +16,66 @@ void coDissolveFlatVertices(coHalfEdgeMesh& mesh, coFloat tolerance)
 		coHalfEdge& a1 = edges[a1Idx];
 		const coUint32 a0Idx = a1.prev;
 		const coUint32 a2Idx = a1.next;
-		if (a0Idx != a1Idx && a0Idx != a2Idx)
+		if (a0Idx == a1Idx || a1Idx == a2Idx)
+			continue;
+			
+		coHalfEdge& a0 = edges[a0Idx];
+		coHalfEdge& a2 = edges[a2Idx];
+		const coVec3& a0Pos = vertices[a0.vertexIdx];
+		const coVec3& a1Pos = vertices[a1.vertexIdx];
+		const coVec3& a2Pos = vertices[a2.vertexIdx];
+		const coVec3 dirA01 = coNormalize(a1Pos - a0Pos);
+		const coVec3 dirA12 = coNormalize(a2Pos - a1Pos);
+		if (coAbs(coDot(dirA01, dirA12) - 1.0f) > tolerance)
+			continue;
+
+		if (a0.nextRadial == a0Idx)
 		{
-			coHalfEdge& a0 = edges[a0Idx];
-			coHalfEdge& a2 = edges[a2Idx];
-			const coVec3& a0Pos = vertices[a0.vertexIdx];
-			const coVec3& a1Pos = vertices[a1.vertexIdx];
-			const coVec3& a2Pos = vertices[a2.vertexIdx];
-			const coVec3 dirA01 = coNormalize(a1Pos - a0Pos);
-			const coVec3 dirA12 = coNormalize(a2Pos - a1Pos);
-			if (coAbs(coDot(dirA01, dirA12) - 1.0f) > tolerance)
+			if (a1.nextRadial == a1Idx)
 			{
+				// Link 0 & 2
+				a0.next = a2Idx;
+				a2.prev = a0Idx;
+
+				// Remove 1
+				a1.prev = a1Idx;
+				a1.next = a1Idx;
+			}
+		}
+		else if (a1.nextRadial != a1Idx)
+		{
+			const coUint32 b1Idx = a0.nextRadial;
+			coHalfEdge& b1 = edges[b1Idx];
+			const coUint32 b0Idx = b1.prev;
+			if (b0Idx != a1.nextRadial)
 				continue;
-			}
 
-			if (a0.nextRadial == a0Idx)
-			{
-				if (a1.nextRadial == a1Idx)
-				{
-					// Link 0 & 2
-					a0.next = a2Idx;
-					a0.nextRadial = a1.nextRadial;
-					a0.prevRadial = a1.prevRadial;
-					a2.prev = a0Idx;
+			coHalfEdge& b0 = edges[b0Idx];
+			const coUint32 b2Idx = b1.next;
+			if (b2Idx == b1Idx)
+				continue;
 
-					// Remove 1
-					a1.prev = a1Idx;
-					a1.next = a1Idx;
-				}
-			}
-			else if (a1.nextRadial != a1Idx)
-			{
-				const coUint32 b1Idx = a0.nextRadial;
-				coHalfEdge& b1 = edges[b1Idx];
-				const coUint32 b0Idx = b1.prev;
-				if (b0Idx != b1Idx)
-				{
-					coHalfEdge& b0 = edges[b0Idx];
-					const coUint32 b2Idx = b1.next;
-					if (b2Idx != b1Idx)
-					{
-						coHalfEdge& b2 = edges[b2Idx];
-						if (a0.nextRadial == b1Idx && a1.nextRadial == b0Idx)
-						{
-							// Link 0 & 2
-							a0.next = a2Idx;
-							a0.nextRadial = a1.nextRadial;
-							a0.prevRadial = a1.prevRadial;
-							a2.prev = a0Idx;
-							b0.next = b2Idx;
-							b0.nextRadial = b1.nextRadial;
-							b0.prevRadial = b1.prevRadial;
-							b2.prev = b0Idx;
+			coHalfEdge& b2 = edges[b2Idx];
 
-							// Remove 1
-							a1.prev = a1Idx;
-							a1.next = a1Idx;
-							b1.prev = b1Idx;
-							b1.next = b1Idx;
-						}
-					}
-				}
-			}
+			// Link 0 & 2
+			a0.next = a2Idx;
+			a0.nextRadial = b0Idx;
+			a0.prevRadial = b0Idx;
+			a2.prev = a0Idx;
+			b0.next = b2Idx;
+			b0.nextRadial = a0Idx;
+			b0.prevRadial = a0Idx;
+			b2.prev = b0Idx;
+
+			// Remove 1
+			a1.prev = a1Idx;
+			a1.next = a1Idx;
+			a1.nextRadial = a1Idx;
+			a1.prevRadial = a1Idx;
+			b1.prev = b1Idx;
+			b1.next = b1Idx;
+			b1.nextRadial = b1Idx;
+			b1.prevRadial = b1Idx;
 		}
 	}
 }
