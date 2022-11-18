19438450ef5072f90f1c3b8d85e7f57944b63319
Jounayd Id Salah <smogpill@gmail.com>
2022-11-19 00:03:59 +0100

    sxdfsdf

--- src/ecs/entity/coEntityHandle.cpp
+++ src/ecs/entity/coEntityHandle.cpp
@@ -3,37 +3,6 @@
 #include "ecs/pch.h"
 #include "coEntityHandle.h"
 #include <pattern/uuid/coUuid_f.h>
-#include "../coEntitySystem.h"
 #include "coEntity.h"
 
 const coEntityHandle coEntityHandle::invalid;
-
-/*
-void coEntityHandle::Write(coBinaryOutputStream& stream) const
-{
-	coASSERT(coEntitySystem::instance);
-	const coEntity* entity = coEntitySystem::instance->Get(*this);
-	stream << entity->GetUuid();
-}
-
-void coEntityHandle::Read(coBinaryInputStream& stream)
-{
-	coUuid uuid;
-	stream >> uuid;
-	coASSERT(coEntitySystem::instance);
-	*this = coEntitySystem::instance->Get(uuid);
-}
-
-void coEntityHandle::DeepWrite(coBinaryOutputStream& stream) const
-{
-	const coEntity* entity = coEntitySystem::instance->Get(*this);
-	stream << *entity;
-}
-
-void coEntityHandle::DeepRead(coBinaryInputStream& stream)
-{
-	coEntity* entity = new coEntity();
-	stream >> *entity;
-	*this = coEntitySystem::instance->Give(*entity);
-}
-*/
